b919a2985accca95eb1cbc75d74d496c284b14c2
Islem Mahdi <islemmahdi0@gmail.com>
2023-11-29 20:34:13 +0100

    commit to mouna
    commit to mouna

---
+++ projet_Gestion_Rh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projet_Gestion_Rh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ projet_Gestion_Rh/Models/RhDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projet_Gestion_Rh.Models
+{
+    public class RhDBContext : DbContext
+    {
+        public RhDBContext(DbContextOptions<RhDBContext> options)
+    : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    => optionsBuilder.UseSqlServer("Server=DESKTOP-ASL6U64\\MSSQLSERVER1;Database=RhDB;Integrated Security=True;Trust Server Certificate=yes");
+
+
+
+
+    }
+}
