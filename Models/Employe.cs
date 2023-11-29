a231d7141605f157ec217ec084dec219b0ee5231
Islem Mahdi <islemmahdi0@gmail.com>
2023-11-29 20:53:52 +0100

    commit hello

--- Models/Employe.cs
+++ Models/Employe.cs
@@ -1,6 +1,19 @@
-﻿namespace projet_Gestion_Rh.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projet_Gestion_Rh.Models
 {
     public class Employe
     {
+        [Key]
+        [Column (TypeName="int")]
+        public int EmployeId { get; set; }
+
+        [Required(ErrorMessage = "le nom est obligatoire")]
+        [MinLength(3, ErrorMessage = "Le nom doir comporter au moins 3 caractere")]
+        public string nomPrenom { get; set; }
+        public String DateNaissance { get; set; }
+
+
     }
 }
--- Models/RhDBContext.cs
+++ Models/RhDBContext.cs
@@ -9,10 +9,7 @@ namespace projet_Gestion_Rh.Models
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseSqlServer("Server=DESKTOP-ASL6U64\\MSSQLSERVER1;Database=RhDB;Integrated Security=True;Trust Server Certificate=yes");
-
-
+        public DbSet<Employe> Employe { get; set; }
 
 
     }
